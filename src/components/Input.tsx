9d4e8e22480bf58488989a54b6d0b2d14c1a8b15
Mark Pineda <mfpc1500@gmail.com>
2023-05-28 16:00:45 -0500

    login funcional

---
+++ src/components/Input.tsx
@@ -0,0 +1,95 @@
+import { View, Text, TextInput, Keyboard } from 'react-native'
+import React from 'react'
+import { FontAwesome5 } from '@expo/vector-icons';
+import { Motion } from "@legendapp/motion"
+import { useColorScheme } from 'nativewind';
+
+interface Props{
+  name: string;
+  value: string;
+  onChange: (text: string) => void;
+  error?: string;
+  icon?: string;
+  reference?: React.RefObject<TextInput>;
+  onSubmitEditing?: () => void;
+}
+
+const Input = ({name, value, onChange, error, icon, reference, onSubmitEditing}: Props) => {
+  const [focus, setFocus] = React.useState(false)
+  const {colorScheme} = useColorScheme();
+  const input = React.useRef<TextInput>(null);
+
+  const hasValues = () => {
+    if(value) return true;
+    return false;
+  }
+
+  Keyboard.addListener('keyboardDidHide', () => {
+    setFocus(false);
+    input.current?.blur();
+    reference?.current?.blur();
+  })
+    
+  return (
+    <View>
+      <View>
+        <View className="absolute top-0 left-2">
+          <FontAwesome5 
+            name={icon} size={24} 
+            color={focus 
+              ? colorScheme === "light" 
+                ? "#006FFF" 
+                : "#348CFF" 
+              : colorScheme === "light" 
+                ? "#000" 
+                : error
+                  ? "red" 
+                  : "#fff"
+            } 
+          />
+        </View>
+        <Motion.View
+          className="absolute left-10 pointer-events-none"
+          animate={{y: hasValues() ? -10 : focus ? -10 : 0}}
+          pointerEvents="none"
+        >
+          <Motion.Text
+            animate={{
+              fontSize: focus 
+                ? 12 
+                : 16, 
+              color: focus 
+                ? colorScheme === "light" 
+                  ? "#006FFF" 
+                  : "#348CFF" 
+                : colorScheme === "light" 
+                  ? "#000" 
+                  : error
+                    ? "red" 
+                    : "#fff"
+            }}
+          >
+            {name}
+          </Motion.Text>
+        </Motion.View>
+        <TextInput 
+          ref={reference ?? input}
+          value={value} 
+          onChangeText={onChange} 
+          className='w-full h-8 border-b border-stone-200 pl-10'
+          onBlur={() => value === "" ? setFocus(false) : null}
+          onFocus={() => setFocus(true)}
+          blurOnSubmit={false}
+          onSubmitEditing={onSubmitEditing}
+        />
+        <Motion.View 
+          className="absolute bottom-0 left-0 h-0.5 w-full border"
+          style={{ borderColor: focus ? colorScheme === "light" ? "#006FFF" : "#348CFF" : error ? "red" : "#000"}}
+          animate={{scaleX: hasValues() ? 1 : focus ? 1: 0}}/>
+      </View>
+      {error && <Text className='text-red-500'>{error}</Text>}
+    </View>
+  )
+}
+
+export default Input
--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -1,57 +1,95 @@
 import React from 'react';
 import auth from '@react-native-firebase/auth';
-import { View, Text, TextInput, Pressable } from 'react-native';
-import NeonCircle from '../components/NeonCircle';
+import { View, Text, TextInput, Pressable, Keyboard } from 'react-native';
+import { BlurView } from '@react-native-community/blur';
+import { useColorScheme } from 'nativewind';
+import Input from '../components/Input';
 
 export default function LoginScreen() {
 
   const [email, setEmail] = React.useState('');
   const [password, setPassword] = React.useState('');
+  const [emailError, setEmailError] = React.useState('');
+  const [passwordError, setPasswordError] = React.useState('');
+  const [isLogin, setIsLogin] = React.useState(false);
+  const refNext = React.useRef<TextInput>(null);
 
+  const {colorScheme} = useColorScheme();
+
+  /**
+   * Funcion para iniciar sesion del usuario
+   * 
+   * @param email Correo electronico del usuario
+   * @param password Contraseña del usuario
+   * 
+   * @error auth/invalid-email Si el correo electronico no es valido
+   * @error auth/user-disabled Si el usuario esta deshabilitado
+   * @error auth/user-not-found Si el usuario no existe
+   * @error auth/wrong-password Si la contraseña es incorrecta
+   * 
+   */
   function doLogin() {
+    setIsLogin(true);
     auth()
       .signInWithEmailAndPassword(email, password)
       .then(() => {
+        setIsLogin(false);
         console.log('User account signed in!');
       })
       .catch(error => {
+        setIsLogin(false);
+        if(error.code === 'auth/invalid-email') setEmailError("Email invalido");
+        if(error.code === 'auth/user-disabled') setEmailError("Usuario deshabilitado");
+        if(error.code === 'auth/user-not-found') setEmailError("Usuario no encontrado");
+        if(error.code === 'auth/wrong-password') setPasswordError("Contraseña incorrecta");
         console.error(error);
       })
   }
 
+  Keyboard.addListener('keyboardDidShow', () => {
+    setEmailError('');
+    setPasswordError('');
+  })
+
   return (
-    <View tw='relative bg-white dark:bg-darkPrimary h-screen flex flex-col items-center justify-center'>
-      <View tw={`absolute z-0 top-[-20%] left-[-45%] bg-emerald-600 h-[400px] rounded-full flex justify-center items-center`} style={{aspectRatio: 1}}>
-        <View tw={`bg-emerald-600 h-[460px] opacity-75 rounded-full flex justify-center items-center`} style={{aspectRatio: 1}}>
-          <View tw={`bg-emerald-600 h-[520px] opacity-50 rounded-full flex justify-center items-center`} style={{aspectRatio: 1}}>
-            <View tw={`bg-emerald-600 h-[600px] opacity-25 rounded-full`} style={{aspectRatio: 1}}>
+    <View className='relative bg-white dark:bg-darkPrimary h-screen flex flex-col items-center justify-center'>
+      <View className={`absolute z-0 top-[-20%] left-[-45%] bg-emerald-600 h-[400px] rounded-full flex justify-center items-center`} style={{aspectRatio: 1}}>
+        <View className={`bg-emerald-600 h-[460px] opacity-75 rounded-full flex justify-center items-center`} style={{aspectRatio: 1}}>
+          <View className={`bg-emerald-600 h-[520px] opacity-50 rounded-full flex justify-center items-center`} style={{aspectRatio: 1}}>
+            <View className={`bg-emerald-600 h-[600px] opacity-25 rounded-full`} style={{aspectRatio: 1}}>
             </View>
           </View>
         </View>
       </View>
-      <View tw={`absolute z-0 top-1/3 right-[-20%] bg-blue-800 h-[250px] rounded-full flex justify-center items-center`} style={{aspectRatio: 1}}>
-        <View tw={`bg-blue-800 h-[288px] opacity-75 rounded-full flex justify-center items-center`} style={{aspectRatio: 1}}>
-          <View tw={`bg-blue-800 h-[325px] opacity-50 rounded-full flex justify-center items-center`} style={{aspectRatio: 1}}>
-            <View tw={`bg-blue-800 h-[375px] opacity-25 rounded-full`} style={{aspectRatio: 1}}>
+      <View className={`absolute z-0 top-1/3 right-[-20%] bg-blue-800 h-[250px] rounded-full flex justify-center items-center`} style={{aspectRatio: 1}}>
+        <View className={`bg-blue-800 h-[288px] opacity-75 rounded-full flex justify-center items-center`} style={{aspectRatio: 1}}>
+          <View className={`bg-blue-800 h-[325px] opacity-50 rounded-full flex justify-center items-center`} style={{aspectRatio: 1}}>
+            <View className={`bg-blue-800 h-[375px] opacity-25 rounded-full`} style={{aspectRatio: 1}}>
             </View>
           </View>
         </View>
       </View>
-      <View tw={`absolute z-0 bottom-0 left-[-50%] bg-red-800 h-[300px] rounded-full flex justify-center items-center`} style={{aspectRatio: 1}}>
-        <View tw={`bg-red-800 h-[345px] opacity-75 rounded-full flex justify-center items-center`} style={{aspectRatio: 1}}>
-          <View tw={`bg-red-800 h-[390px] opacity-50 rounded-full flex justify-center items-center`} style={{aspectRatio: 1}}>
-            <View tw={`bg-red-800 h-[451px] opacity-25 rounded-full`} style={{aspectRatio: 1}}>
+      <View className={`absolute z-0 bottom-0 left-[-50%] bg-red-800 h-[300px] rounded-full flex justify-center items-center`} style={{aspectRatio: 1}}>
+        <View className={`bg-red-800 h-[345px] opacity-75 rounded-full flex justify-center items-center`} style={{aspectRatio: 1}}>
+          <View className={`bg-red-800 h-[390px] opacity-50 rounded-full flex justify-center items-center`} style={{aspectRatio: 1}}>
+            <View className={`bg-red-800 h-[451px] opacity-25 rounded-full`} style={{aspectRatio: 1}}>
             </View>
           </View>
         </View>
       </View>
-      <View tw='w-32 h-44 rounded-xl bg-white opacity-20 border border-3 border-white'>
+      <View className='h-44 w-38 border-1 border-neutral-500 rounded-lg overflow-hidden flex items-center justify-center'>
+        <BlurView style={{width: 126, height: 174, }} blurType="light" blurAmount={15}>
+        </BlurView>
       </View>
-      <View tw='relative w-11/12 translate-x-10 bg-white dark:bg-darkSeconday h-60 mt-20' style={{elevation: 10}}>
-        <TextInput value={email} onChangeText={setEmail} placeholder='Email...'/>
-        <TextInput value={password} onChangeText={setPassword} placeholder='Password...'/>
-        <Pressable onPress={doLogin} tw='bg-black dark:bg-white absolute bottom-[-20px] right-[30px] pl-12 pr-10 py-2'>
-          <Text tw='text-white dark:text-black uppercase font-bold text-xl'>Ingresar</Text>
+      <View className='relative w-11/12 translate-x-10 bg-white dark:bg-darkSeconday h-60 mt-20 p-10 flex flex-col' style={{elevation: 10, gap: 25}} >
+        <Input icon="user-alt" name='Correo Electronico' value={email} onChange={setEmail} error={emailError} onSubmitEditing={() => refNext.current?.focus()}/>
+        <Input icon="lock" name='Contraseña' value={password} onChange={setPassword} error={passwordError} reference={refNext} onSubmitEditing={doLogin}/>
+        <Text className='self-end dark:text-white'>Recuperar Contraseña</Text>
+        <Pressable onPress={doLogin} className='bg-black dark:bg-white absolute bottom-[-20px] right-[20px] pl-12 pr-10 py-2'>
+          {isLogin 
+            ? <Text className='text-white dark:text-black uppercase font-bold text-xl'>Cargando...</Text> 
+            : <Text className='text-white dark:text-black uppercase font-bold text-xl'>Ingresar</Text>
+          }
         </Pressable>
       </View>
     </View>
